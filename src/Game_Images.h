e5212411ce9a2d569d6cafa201c529052bb78616
ABDULHADI\Abdulhadi <aa6moham@uwaterloo.ca>
2017-01-23 20:20:04 -0500

    added more documentation + changed file structure

---
+++ src/Game_Images.h
@@ -0,0 +1,58 @@
+extern const unsigned char SmallEnemy30PointA[];
+
+extern const unsigned char SmallEnemy30PointB[];
+
+extern const unsigned char SmallEnemy20PointA[];
+
+extern const unsigned char SmallEnemy20PointB[];
+
+extern const unsigned char SmallEnemy10PointA[];
+
+extern const unsigned char SmallEnemy10PointB[];
+
+extern const unsigned char PlayerShip0[];
+
+extern const unsigned char SmallEnemyBonus0[];
+
+
+extern const unsigned char Bunker0[] ;
+
+
+extern const unsigned char Bunker1[] ;
+
+
+extern const unsigned char Bunker2[];
+
+
+extern const unsigned char Bunker3[];
+
+
+extern const unsigned char BigExplosion0[];
+
+
+extern const unsigned char BigExplosion1[] ;
+
+extern const unsigned char SmallExplosion0[];
+
+extern const unsigned char SmallExplosion1[];
+
+extern const unsigned char Missile0[];
+
+extern const unsigned char Missile1[];
+
+extern const unsigned char Missile2[];
+
+extern const unsigned char Laser0[];
+
+extern const unsigned char Laser1[];
+
+extern const unsigned char Points10[];
+extern const unsigned char Points20[];
+
+extern const unsigned char Points30[];
+
+extern const unsigned char Points50[];
+
+extern const unsigned char Heart[];
+
+extern const unsigned char empty_heart[];
